0d553a7ecf75b0ffa9fc515e7d2bc9f01cbdf83c
julio <julio458.h@gmail.com>
2021-10-06 16:09:05 -0600

    v1.0 consultas-jpa

---
+++ src/main/java/com/gm/sga/servicio/PersonaServiceImpl.java
@@ -0,0 +1,46 @@
+package com.gm.sga.servicio;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import com.gm.sga.datos.PersonaDao;
+import com.gm.sga.domain.Persona;
+
+@Stateless
+public class PersonaServiceImpl implements PersonaServiceRemote, PersonaService{
+
+    @Inject
+    private PersonaDao personaDao;
+    
+    @Override
+    public List<Persona> listarPersonas() {
+       return personaDao.findAllPersonas();
+    }
+
+    @Override
+    public Persona encontrarPersonaPorId(Persona persona) {
+        return personaDao.findPersonaById(persona);
+    }
+
+    @Override
+    public Persona encontrarPersonaPorEmail(Persona persona) {
+        return personaDao.findPersonaByEmail(persona);
+    }
+
+    @Override
+    public void registrarPersona(Persona persona) {
+        personaDao.insertPersona(persona);
+    }
+
+    @Override
+    public void modificarPersona(Persona persona) {
+        personaDao.updatePersona(persona);
+    }
+
+    @Override
+    public void eliminarPersona(Persona persona) {
+        personaDao.deletePersona(persona);
+    }
+    
+}
